c9dd6bee2d25290a01aa37cf27564d7858ab0b8a
S4mur419020 <muraitomi04@gmail.com>
2025-12-26 16:38:05 +0100

    api hivasok

--- Sneakers_Backend/routes/api.php
+++ Sneakers_Backend/routes/api.php
@@ -1,8 +1,17 @@
 <?php
 
+use App\Http\Controllers\KategoriakController;
+use App\Http\Controllers\MarkakController;
+use App\Http\Controllers\TermekekController;
+use App\Http\Controllers\TermekValtozatokController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/kategoriak', [KategoriakController::class, 'index']);
+Route::get('/markak', [MarkakController::class, 'index']);
+Route::get('/termekek', [TermekekController::class, 'index']);
+Route::get('/termek_valtozatok', [TermekValtozatokController::class, 'index']);
